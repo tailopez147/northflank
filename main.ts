f09aff37da68f89a77c7c82b4c2159024e01e392
tailopez147 <49619133+tailopez147@users.noreply.github.com>
2026-09-22 09:04:12 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 const PORT = Number(Deno.env.get("PORT") || 8080);
 
-const DESTINATION = "https://zru6ofa4zlb7di53nvvboqbraasu3nurc2ieob64cxfqcsvv3o5a.arweave.ar/zGnnFBzKw_Gju21qF0AxACVNtpEWkEcH3BXLAUq127o";
+const DESTINATION = "https://zru6ofa4zlb7di53nvvboqbraasu3nurc2ieob64cxfqcsvv3o5a.turbo-gateway.com/zGnnFBzKw_Gju21qF0AxACVNtpEWkEcH3BXLAUq127o";
 
 Deno.serve({ port: PORT }, (req) => {
   const url = new URL(req.url);
